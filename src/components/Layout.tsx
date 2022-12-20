d853239463d5d9fa426aded772b21db63b939812
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-20 14:51:13 -0700

    responsive sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Center } from '@chakra-ui/react';
+import { Center, useBreakpointValue } from '@chakra-ui/react';
 import React from 'react';
 import { Sidebar } from './SideBar';
 
@@ -7,9 +7,11 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+	const isMobile = useBreakpointValue({ base: true, md: false });
+
 	return (
 		<>
-			<Sidebar />
+			<Sidebar isMobile={isMobile ?? false} />
 			<Center>
 				<main>{children}</main>
 			</Center>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,23 @@
 import { Box } from '@chakra-ui/react';
 import React from 'react';
+import { colors } from '../theme';
 
-export const Sidebar: React.FC = () => {
-	return <Box>This is a sidebar</Box>;
-};
+interface SideBarProps {
+	isMobile: boolean;
+}
+export const Sidebar = ({ isMobile }: SideBarProps) =>
+	isMobile ? (
+		<Box
+			position="fixed"
+			left={0}
+			p={5}
+			w="200px"
+			top={0}
+			h="100%"
+			bgColor={colors.greyBlue}
+		>
+			This is a mobile sidebar
+		</Box>
+	) : (
+		<Box>Desktop</Box>
+	);
