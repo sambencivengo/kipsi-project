4827d16dd1d1cc0349ec0d0689a39f0264e67a91
Sam Bencivengo <sambencivengo@gmail.com>
2022-12-21 13:28:02 -0700

    server set up, listening

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 8000;
+
+const start = async () => {
+	const app = express();
+
+	app.get('/', (_, res) => {
+		res.send('Hello world');
+	});
+
+	app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+};
+start();
